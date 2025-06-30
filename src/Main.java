5b851468e56cb8c021a6f9d34f45ca3c4a57fab2
fieldryan <ryanfieldnz@gmail.com>
2025-06-30 21:11:24 +1200

    initial

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    record Card(String id, int value){}
+
+    public static String theTruth(){
+        return "Ryan is awesome";
+    }
+    public static void main(String[] args) {
+        System.out.println(theTruth());
+        System.out.println("bean");
+
+    }
+
+
+}
+
+
